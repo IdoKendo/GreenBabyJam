ff1dcee262a0f4baca7976b9e6e3e3c3163cbbb5
Omer Ben Yosef <bomberby@gmail.com>
2018-12-01 00:49:22 +0900

    Fireball door (#2)
    * WIP fireball-door interaction
    
    * bullet-hell scene: Fix scene location

---
+++ Assets/Scripts/Burnable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Burnable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        Burn();
+    }
+
+    void Burn()
+    {
+        ParticleSystem fire = GetComponentInChildren<ParticleSystem>();
+        fire.Play();
+        Debug.Log("HELP I'M BURNING");
+    }
+}

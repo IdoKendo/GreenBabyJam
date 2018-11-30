680ee67c6e5c2a35d1670e3c6512c9ab318fed78
Idan Agamy <idanagamy@gmail.com>
2018-12-01 01:59:00 +0200

    Add HealthBar UI

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+
+    private Transform bar;
+    public Transform healthBar;
+   
+    private Player player;
+
+    
+
+    // Use this for initialization
+    void Start () {
+        bar = transform.Find("Bar");
+        healthBar = transform.Find("HealthBar");
+        player = FindObjectOfType<Player>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+        //if (player != null)
+        //{
+            float normilizedHealth = player.Health / player.MaxHealth;
+            bar.localScale = new Vector2(normilizedHealth, 1f);
+        //}
+
+
+    }
+    
+}

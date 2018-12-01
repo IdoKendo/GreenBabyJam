394eed9444209808a61d52bf4209a1c5c575b1b7
IdoKendo <ryuusuke@gmail.com>
2018-12-01 14:08:24 +0200

    Change fireball direction according to where the player is facing

--- Assets/Scripts/PlayerScript/Fireball.cs
+++ Assets/Scripts/PlayerScript/Fireball.cs
@@ -6,12 +6,12 @@ public class Fireball : DamageDealer
 {
     [Header("Movement")]
     [SerializeField] private float m_speed = 8f;
-    [SerializeField] private int m_direction = 1;
+    [SerializeField] private EDirection m_direction = EDirection.Right;
     [SerializeField] private float m_upSpeed = 2;
 
     [Header("Stats")]
     [SerializeField] private float m_lifetime = 5f;
-
+    
     private Rigidbody2D m_rigidBody;
 
     private void Start()
@@ -23,10 +23,22 @@ public class Fireball : DamageDealer
 
     private void FixedUpdate()
     {
+        float xVector = Vector2.left.x;
+
         m_upSpeed -= 0.08f;
 
-        Vector2 movement = new Vector2(m_direction, m_upSpeed);
+        if (m_direction == EDirection.Left)
+        {
+            xVector = Vector2.right.x;
+        }
+
+        Vector2 movement = new Vector2(xVector, m_upSpeed);
         
         m_rigidBody.AddForce(movement * m_speed);
+    }
+
+    public void SetDirection(EDirection direction)
+    {
+        m_direction = direction;
     }
 }

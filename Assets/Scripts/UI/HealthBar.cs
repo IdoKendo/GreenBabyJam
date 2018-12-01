4a0cdd7d1277d0f4dbaf1b8a97ac06c103d71641
Idan Agamy <idanagamy@gmail.com>
2018-12-01 12:46:15 +0200

    fix postion error, changed to transperent backgroind in UI

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -1,33 +1,44 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthBar : MonoBehaviour {
 
+    private GameObject healthBar;
     private Transform bar;
    
     private Player player;
 
-    
+    private const float healthBarXPosition = 0.55f;
+    private const float healthBarYPosition = 0.54f;
 
     // Use this for initialization
     void Start () {
         bar = transform.Find("Bar");
         player = FindObjectOfType<Player>();
-        
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        
-
-        //if (player != null)
-        //{
-            float normilizedHealth = player.Health / player.MaxHealth;
-            bar.localScale = new Vector2(normilizedHealth, 1f);
-        //}
+        healthBar = GameObject.Find("HealthBar");
+        HealthBarPosition();
 
 
     }
-    
+
+    private void HealthBarPosition()
+    {
+        RectTransform healthBarRect = healthBar.transform as RectTransform;
+        healthBarRect.anchorMax = new Vector2(healthBarXPosition, healthBarYPosition);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        ChangeHealt();
+    }
+
+    public void ChangeHealt()
+    {
+        float normilizedHealth = player.Health / player.MaxHealth;
+
+        normilizedHealth = Mathf.Clamp(normilizedHealth, 0, 1);
+        bar.localScale = new Vector2(normilizedHealth, 1f);
+    }
 }

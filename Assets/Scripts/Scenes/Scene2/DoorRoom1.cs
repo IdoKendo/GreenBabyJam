ab43ba99b516618ac196c7123a2b76bb43b50e0e
Or Perlman <or.perlman@trader.ca>
2018-12-01 23:30:01 -0800

    Level 2 Changes: 1. Added a collision for door that leads back to level 1, for returning.

---
+++ Assets/Scripts/Scenes/Scene2/DoorRoom1.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Scripts.Shared.Enums;
+using Shared.Enums;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Scenes.Scene1
+{
+    public class DoorRoom1 : MonoBehaviour
+    {
+        public Animator transitionAnimation;
+
+        public void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag(GameCollisionType.PLAYER))
+                StartCoroutine(LoadScene());
+        }
+
+        IEnumerator LoadScene()
+        {
+            var player = FindObjectOfType<Player>();
+            player.Save();
+
+            transitionAnimation.SetTrigger(AnimationTriggerType.SCREEN_FADE_END);
+            yield return new WaitForSeconds(0.2f);
+            SceneManager.LoadScene((int)SceneType.LEVEL1);
+        }
+    }
+}
